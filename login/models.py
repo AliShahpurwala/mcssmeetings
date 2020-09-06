189e1d24ff161e74780ba0b43056fc7e795265ea
AliShahpurwala <ali.murtaza.am@gmail.com>
2020-09-05 22:43:05 -0400

    User model and User Model Manager created

--- login/models.py
+++ login/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import (
+	AbstractBaseUser,
+	BaseUserManager
+	)
 
+import django.utils.timezone
 # Create your models here.
+
+class UserManager(BaseUserManager):
+	def create_user(self, username, password):
+		
+		username = self.normalize_username(username)
+		user = self.model(username=username)
+		user.set_password(password)
+		user.save()
+
+		return user
+
+class User(AbstractBaseUser):
+	username = models.CharField(max_length=255, primary_key=True)
+	administratorStatus = models.BooleanField(default=False)
+	timeCreated = models.DateTimeField(default=timezone.now)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = []
+
