4115e49fd367fb91dd6bd7064a17646f2f5d49be
AliShahpurwala <ali.murtaza.am@gmail.com>
2020-09-10 17:05:21 -0400

    Created Meeting Model. Model working properly. Meetings Page needs to be created next.

--- meeting/models.py
+++ meeting/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from login.models import User
 # Create your models here.
+class meeting(models.Model):
+	startTime = models.DateTimeField(blank=False)
+	endTime = models.DateTimeField(blank=False)
+	createdOn = models.DateTimeField(auto_now_add=True)
+	attendedBy = models.ManyToManyField(User)
