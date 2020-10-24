b5d15ca1dbb6bde595cb3052a01b816f0d27a55f
AliShahpurwala <ali.murtaza.am@gmail.com>
2020-10-24 11:44:36 -0400

    V1.0.0 ready for deployment

--- mcssmeetings/settings.py
+++ mcssmeetings/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kq*hihw7_*cx^v+^xuuksy8xec$3273mvbx1t(n+g$o6n&-f49'
+f = open("secret_key", "r")
+secret_key_from_file = f.read()
+
+SECRET_KEY = secret_key_from_file   # 'kq*hihw7_*cx^v+^xuuksy8xec$3273mvbx1t(n+g$o6n&-f49'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
